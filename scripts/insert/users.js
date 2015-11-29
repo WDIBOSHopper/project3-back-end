1b4ad478cb5e124df3f27506213779a3468b21ef
Lauren Grant <lgrant1286@gmail.com>
2015-11-29 13:24:24 -0500

    Inserts 3 users to the Users collection in Mongo db.
    Can view users id in Mongo. Example passwords are not encripted at this time.

---
+++ scripts/insert/users.js
@@ -0,0 +1,17 @@
+db.users.insert([
+  {
+    username: 'Lauren',
+    email: 'lauren@lauren.com',
+    password: '123'
+  },
+  {
+    username: 'Rachel',
+    email: 'rachel@rachel.com',
+    password: '123'
+  },
+  {
+    username: 'Bill',
+    email: 'bill@bill.com',
+    password: '123'
+  }
+]);
