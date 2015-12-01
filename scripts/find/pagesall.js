dba29f4f0f7341f4a102e367a805fc11ac897d86
bbarber <bbarber@coastalcomputerservices.net>
2015-12-01 10:04:25 -0500

    Finds pages by user_id.
    * Tested in Node.
    * Pair Programming RTS, LG, BB

---
+++ scripts/find/pagesall.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/mongo-crud');
+var db = mongoose.connection;
+
+var Page = require('../../models/Page.js');
+
+var done = function() {
+  db.close();
+};
+
+var showPage = function(user_id) {
+  console.log(user_id);
+  Page.findOne({'user_id': user_id})
+  .exec()
+  .then(function(page) {
+    console.log("THIS BLOG LIST IS FOR A SINGLE USER");
+    console.log(page);
+  }).catch(function(error){
+    console.error(error);
+  }).then(done);
+};
+
+db.once('open', function(){
+  var user_id = process.argv[2];
+
+  var page = showPage(user_id);
+});
