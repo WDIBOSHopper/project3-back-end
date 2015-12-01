6aaa9a5684262cea62b48a93909f5f4c3951615e
bbarber <bbarber@coastalcomputerservices.net>
2015-11-30 23:57:10 -0500

    created scripts/update/posts.js to update a post based on its post_id. the update script takes up to three arguments - one is the search index (in this case the post_id) & the other three are title and entry.
    Also implemnted upsert functionality to create a new post if the post_id
    does not exists has not been added.
    
    ALso modified the scripts/update/pages.js with upsert functionality.
    
    * File was tested using mongo db & node executing the script files.
    
    * file was created by BB

--- scripts/update/posts.js
+++ scripts/update/posts.js
@@ -3,35 +3,33 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/mongo-crud');
 var db = mongoose.connection;
 
-var Page = require('../../models/Page.js');
+var Post = require('../../models/Post.js');
 
 var done = function() {
   db.close();
 };
 
-var updatePage = function(page_id, title, content, url) {
-  Page.update(
-    { _id: page_id },
+var updatePost = function(post_id, title, entry) {
+  Post.update(
+    { _id: post_id },
     {
       $set: {
         'title': title,
-        'content': content,
-        'url': url
+        'entry': entry
       }
     },
     { upsert: true }
   )
-  .then(function(page) {
-    console.log("THIS BLOG WAS UPDATED");
+  .then(function(post) {
+    console.log("POST WAS UPDATED");
   }).catch(function(error){
     console.error(error);
   }).then(done);
 };
 
 db.once('open', function(){
-  var page_id = process.argv[2];
+  var post_id = process.argv[2];
   var title = process.argv[3];
-  var content = process.argv[4];
-  var url = process.argv[5];
-  var page = updatePage(page_id, title, content, url);
+  var entry = process.argv[4];
+  var post = updatePost(post_id, title, entry);
 });
