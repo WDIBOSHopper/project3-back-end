259cdc84079ae1a0a11b4aa452683fcabd9b01aa
racheltstevens <racheltstevens@gmail.com>
2015-12-02 20:13:10 -0500

    Connects blogRoute to blogController
    * tested in Postman

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var passport = require('passport');
 var router = express.Router();
+var blogController = require('../controllers/blogController');
 
 router.get('/', function(req, res){res.json("it's a real route!");});
 
