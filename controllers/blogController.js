95c45ec1ff95626772a60a2d17284b572c07f6fe
racheltstevens <racheltstevens@gmail.com>
2015-12-02 22:25:26 -0500

    Tests connection between route and controller (blogRoute)
    * works in Postman
    * login still works

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -3,10 +3,29 @@
 var mongoose = require("../models/index");
 
 var Post = mongoose.model("Post");
+var User = mongoose.model("User");
 
 
 var blogController = {
-  get: function(){}
+  // get: function(req, res){
+  //   User.find({'username': req.params.username})
+  //   .exec()
+  //   .then(function(user){
+  //     console.log(user)
+  //     return Post.find({'owner': user[0]._id});
+  //   })
+  //   .then(function(posts){
+  //     res.setStatus(200);
+  //     res.json({posts: posts});
+  //   })
+  //   .catch(function(err){
+  //     res.setStatus(404);
+  //     res.json({message: "Invalid route", error: err});
+  //   });
+  // }
+  get: function(req, res){
+    res.json({message: "It's still a route!"});
+  }
 };
 
 
