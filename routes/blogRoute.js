a54ac83c6c6b6ab885d050ba46471d9f8ba9850f
racheltstevens <racheltstevens@gmail.com>
2015-12-02 22:39:01 -0500

    Trying to fugure out req.params for blogController
    * req.params is an empty object :(
    * login works only after creating a new user

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -3,7 +3,8 @@ var passport = require('passport');
 var router = express.Router();
 var blogController = require('../controllers/blogController');
 
-router.get('/', blogController.get);
+router.get('/', function(req, res){ res.json({username: req.params});
+});
 
 
 module.exports = router;
