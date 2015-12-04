94de1244c8dcb6f50c2fc616322acf8b0ebb2065
Lauren Grant <lgrant1286@gmail.com>
2015-12-04 09:05:29 -0500

    Adds correct filepath to Github pages.

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var app = express();
 app.use(logger('dev'));
 //cors configuration
 app.use(cors({
-  origin: ['http://localhost:5000', 'http://wdiboshopper.github.io/project3-front-end'],
+  origin: 'http://wdiboshopper.github.io',
   credentials: true
 }));
 app.use(bodyParser.json());
